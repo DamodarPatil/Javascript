d8ff0db77064248ab42c9bc4dc7df48c86626b3e
DamodarPatil <patildamodar1983@gmail.com>
2024-06-23 18:52:26 +0530

    .

---
+++ 01_basics/test.js
@@ -0,0 +1,17 @@
+const accountId = 12345;
+let accountEmail = "damodar@gmail.com";
+var accountPassword = "12345";
+accountCity = "Ahmedabad";
+let accountState;
+
+accountEmail = "patil@gmail.com";
+accountPassword = "56356";
+accountCity = "Indore";
+
+console.table([
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
