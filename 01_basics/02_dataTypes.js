afa16c292d60f130ab89ac3b8b1b94f3bdd7a92a
DamodarPatil <patildamodar1983@gmail.com>
2024-06-24 05:56:09 +0530

    .

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) we are using nodejs, not browser
+
+//code readability should be high
+
+console.log(3 + 3);
+
+console.log("Damodar");
+
+let name = "damodar";
+let age = 19;
+let isLoggedIn = false;
+
+// number = 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof "damodar");
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object 
