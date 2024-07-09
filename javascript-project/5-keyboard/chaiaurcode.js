52efe8eeca86e0053b67909bcee657f35f9076ac
DamodarPatil <patildamodar1983@gmail.com>
2024-07-09 18:58:25 +0530

    initial commit

--- javascript-project/5-keyboard/chaiaurcode.js
+++ javascript-project/5-keyboard/chaiaurcode.js
@@ -1,1 +1,20 @@
-console.log('Project 5');
+const insert = document.querySelector("#insert");
+
+window.addEventListener("keydown", (e) => {
+  insert.innerHTML = `
+    <div class = 'color'>
+        <table>
+            <tr>
+                <th>Key</th>
+                <th>keycode</th>
+                <th>Code</th>
+            </tr>
+            <tr>
+                <th>${e.key === " " ? "Space" : e.key}</th>
+                <th>${e.keyCode}</th>
+                <th>${e.code}</th>
+            </tr>
+        </table>
+    </div>
+    `;
+});
